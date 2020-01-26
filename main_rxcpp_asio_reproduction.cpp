21fa229052091a99f795d36d0567b5b50e0b5c51
Kirk Shoop <kirk.shoop@gmail.com>
2020-01-25 16:15:43 -0800

    fixes for async coordination

--- main_rxcpp_asio_reproduction.cpp
+++ main_rxcpp_asio_reproduction.cpp
@@ -47,11 +47,16 @@ struct IoServiceContext
 
 int main(int argc, const char *const argv[]) {
 
+#if 1
+    // minimum changes to work
+    //
+
     static rxcpp::subjects::subject<int> m_picture_processed, m_finished, m_picture;
 
     auto start = std::chrono::system_clock::now();
     IoServiceContext::GetContext();
     std::this_thread::sleep_for(std::chrono::seconds (1));
+    auto asio_coordination = rxcpp::synchronize_in_asio(IoServiceContext::GetContext()->m_ios);
 
     m_picture_processed.get_observable()
             .take(TEST_COUNT)
@@ -63,20 +68,26 @@ int main(int argc, const char *const argv[]) {
                     [start]() {
                         auto duration = int(0.5 + std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count());
                         std::cout << "Duration: " << duration << "\n";
+                        // take() completed this subscription to this subject, 
+                        // now complete all the other subscriptions to all the subjects
+                        m_picture_processed.get_subscriber().on_completed();
+                        m_picture.get_subscriber().on_completed();
                         m_finished.get_subscriber().on_completed();
                     });
 
     m_picture.get_observable()
-            .flat_map([](int i) {
+            .flat_map([=](int i) {
                 return rxcpp::observable<>::defer(
-                        []() {
+                        [=]() {
 //                            std::this_thread::sleep_for(std::chrono::milliseconds (10));
                             std::cout << "Got event\n";
                             m_picture_processed.get_subscriber().on_next(1);
                             return rxcpp::observable<>::just<int>(1);
                         })
-                        .subscribe_on(rxcpp::synchronize_in_asio(IoServiceContext::GetContext()->m_ios));
-            })
+                        .subscribe_on(asio_coordination);
+            }, asio_coordination) // override default - each subscribe_on above is 
+                                  // interleaved across threads and this merges them 
+                                  // back onto one sequence.
             .subscribe();
 
     for (int i=0; i < TEST_COUNT; i++) {
@@ -84,8 +95,84 @@ int main(int argc, const char *const argv[]) {
         m_picture.get_subscriber().on_next(1);
     }
 
-    m_finished.get_observable()
+    // wait for all the subjects to complete
+    m_picture.get_observable()
+            .merge(
+                asio_coordination, // merge the different threads back into one sequence
+                m_picture_processed.get_observable(), 
+                m_finished.get_observable())
             .as_blocking()
             .subscribe();
+
+#else
+    // avoid subjects and prefer operators to subscribe
+    //
+
+    static rxcpp::subjects::subject<int> m_picture_processed, m_picture;
+
+    auto start = std::chrono::system_clock::now();
+    IoServiceContext::GetContext();
+    std::this_thread::sleep_for(std::chrono::seconds (1));
+    auto asio_coordination = rxcpp::synchronize_in_asio(IoServiceContext::GetContext()->m_ios);
+
+    std::cout << "starting processor and listener\n";
+
+    auto proccessor = m_picture_processed.get_observable()
+        .scan(0, [](int sum, int i) { return sum+=i; })
+        .tap([](int i) {
+            std::cout << "Process event " << i << "\n";
+        })
+        .take(TEST_COUNT) // processor completes after all items are processed
+        .finally([start]() {
+            auto duration = int(0.5 + std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - start).count());
+            std::cout << "Duration: " << duration << "\n";
+        })
+        .publish()
+        .connect_forever();
+
+    auto listener = m_picture.get_observable()
+        .tap([](int i) {
+            std::cout << "Sending event " << i + 1 << "\n";
+        })
+        .observe_on(asio_coordination)
+        .tap([](int i) {
+            std::cout << "Listen event "<< i + 1 <<"\n";
+        })
+        .finally([]{
+            std::cout << "listener complete\n";
+        })
+        .map([=](int){ 
+            return rxcpp::sources::defer([=]{
+                return rxcpp::sources::just(1)
+                    .subscribe_on(asio_coordination); 
+            }).as_dynamic();
+        })
+        .merge(asio_coordination) // override default - each subscribe_on above is 
+                                  // interleaved across threads and this merges them 
+                                  // back onto one sequence.
+        .tap(m_picture_processed.get_subscriber()) // forward to the processor
+        .publish()
+        .connect_forever();
+
+    std::cout << "Sending Events\n";
+    for (int i=0; i < TEST_COUNT; i++) {
+        m_picture.get_subscriber().on_next(i);
+    }
+
+    std::cout << "Waiting for processor\n";
+    proccessor
+            .as_blocking()
+            .count();
+
+    std::cout << "close subjects\n";
+    m_picture.get_subscriber().on_completed();
+
+    std::cout << "Waiting for listener\n";
+    listener
+            .as_blocking()
+            .count();
+
+#endif
+
     return 0;
 }
